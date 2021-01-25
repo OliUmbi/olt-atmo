f652a5bb497647148769f2583df0c84c044fb920
OliUmbi <65683302+OliUmbi@users.noreply.github.com>
2021-01-25 20:46:50 +0100

    init

---
+++ src/main/java/ch/oliatmo/Main.java
@@ -0,0 +1,10 @@
+package ch.oliatmo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Loop loop = new Loop();
+        loop.loop();
+    }
+
+}
---
+++ src/main/java/ch/oliatmo/dto/Refresh.java
@@ -0,0 +1,15 @@
+package ch.oliatmo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Refresh {
+    private String access_token;
+    private String refresh_token;
+    private Object scope;
+    private int expires_in;
+    private int expire_in;
+}
+
